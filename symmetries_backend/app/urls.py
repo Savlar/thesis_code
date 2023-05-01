370444ae6c9c4b4172b026a43c151d460c12563c
Savlar <matusgal@outlook.com>
2023-05-01 20:06:53 +0200

    tried removing urls

--- symmetries_backend/app/urls.py
+++ symmetries_backend/app/urls.py
@@ -16,7 +16,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from symmetries_app import urls
+# from symmetries_app import urls
 
 router = DefaultRouter()
 
